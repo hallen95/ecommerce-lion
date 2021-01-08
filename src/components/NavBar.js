b45944fd2ae803abc43f59c8f4471d9d5894711b
Brian <br.y.an@hotmail.com>
2021-01-07 22:23:38 -0300

    tarea3

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Navbar, Nav, Form, FormControl } from 'react-bootstrap';
+import CartWidget from '../components/cartWidget/CartWidget';
+
+const navBar = () => {
+
+return (
+  <Navbar bg="light" expand="lg">
+<Navbar.Brand href="#home">Lion</Navbar.Brand>
+<Navbar.Toggle aria-controls="basic-navbar-nav" />
+<Navbar.Collapse id="basic-navbar-nav">
+  <Nav className="mr-auto">
+    <Nav.Link href="#home">Productos</Nav.Link>
+    <Nav.Link href="#link">Contacto</Nav.Link>
+  </Nav>
+  <Form inline>
+    <CartWidget/>
+    <FormControl type="text" placeholder="buscar" className="mr-sm-2" />
+  </Form>
+</Navbar.Collapse>
+</Navbar>)
+}
+
+export default navBar;
---
+++ src/components/cartWidget/CartWidget.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { FiShoppingCart } from 'react-icons/fi';
+import './cartwidget.css';
+
+ class CartWidget extends React.Component {
+    render() {
+      return <div className="cartStyle"> <FiShoppingCart /> </div>
+    }
+  }
+
+export default CartWidget;
+
+  
+// export const cartWidget = () => {
+//     return (    
+//         <span>
+//             <FiShoppingCart />
+//         </span>
+//     )
+// };
+
