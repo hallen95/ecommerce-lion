7c811744b4264ee0743ab7f01683a65169d587b2
Brian <br.y.an@hotmail.com>
2021-01-31 18:24:49 -0300

    commit en el branch con contador funcionando pero sin estilo

--- src/components/itemDetail/ItemDetail.js
+++ src/components/itemDetail/ItemDetail.js
@@ -1,15 +1,40 @@
-import React from 'react' // rfc 
+import React, { useState } from 'react' // rfc 
 import './itemDetail.css'
+import ItemCount from '../itemCount/ItemCount'
 
 const ItemDetail = ({getItem}) => {
+    let min = 0;
+    const [contador, setContador] = useState(0);
+    const [contadorCarrito, setContadorCarrito] = useState(false)
+
+    const suma = () => {
+        contador >= getItem.stock ? 
+        alert('alerta superaste el stock') 
+        : setContador(contador + 1)
+    }
+
+    const resta = () => {
+        contador > min ?
+        setContador(contador - 1)
+        : alert('el valor no puede ser menor a 0')
+    }
+
+    const onAdd = () => {
+        setContadorCarrito(contadorCarrito + contador);
+        setContador(0);
+        console.log(contadorCarrito);
+        console.log(contador);
+    }
+    
     return (
-        <div>
+        <React.Fragment>
             <h2 className="itemDetailStyle">
                 {getItem.nombre}
             </h2>
             <img className="fotoDetail" src={getItem.imagen} alt="foto del producto Terrnova de Lion"/>
             <div className="DescripcionDetail">{getItem.descripcion}</div>
-        </div>
+            <ItemCount suma={suma} resta={resta} onAdd={onAdd} contador={contador} contadorCarrito={contadorCarrito}/>
+        </React.Fragment>
 
     )   
 }
--- src/components/itemDetailContainer/ItemDetailContainer.js
+++ src/components/itemDetailContainer/ItemDetailContainer.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect } from 'react';
 import ItemDetail from '../itemDetail/ItemDetail';
-import ItemCount from '../itemCount/ItemCount';
 import { useParams } from 'react-router-dom';
 
 
@@ -11,7 +10,8 @@ const productos2 = [{
                         descripcion: 'SU FÓRMULA INNOVADORA SIN SOLVENTES ESTÁ ESPECÍFICAMENTE DISEÑADA PARA OTORGAR PROLONGADA HUMECTACIÓN, DEVOLVER FLEXIBILIDAD Y EL ASPECTO ORIGINAL DE UN CUERO, UN PLÁSTICO O UNA GOMA. DEBIDO A QUE NO OTORGA UN BRILLO EXCESIVO, ES IDEAL PARA EL TORPEDO DEL AUTO PORQUE NO SE GENERA EL REFLEJO EN EL PARABRISAS SU FORMULACIÓN BALANCEADA NO SE TRANSFIERE NI PERMITE QUE SE ADHIERA EL POLVO. TIENE UN AROMA MUY AGRADABLE Y SUAVE. SI SE APLICA EN LOS BURLETES, PROLONGA SU DURABILIDAD Y QUITA RUIDOS GENERADOS POR LOS MISMOS. IDEAL PARA EL USO EN LOS INTERIORES DEL VEHÍCULO. AL NO CONTENER SOLVENTES, NO ENGRASA LOS VIDRIOS.',
                         imagen: 'https://d22fxaf9t8d39k.cloudfront.net/ac8de3383fbcc01fcbe879d0753aa485599f9eff762d2e5904bffea62e03efd36957.png',
                         precio: 1250,
-                        categoria: 'shampoo'
+                        categoria: 'shampoo',
+                        stock: 10
                     },
 
                     {
@@ -21,7 +21,8 @@ const productos2 = [{
                         descripcion: 'ESTE GEL ESTÁ ESPECIALMENTE FORMULADO PARA LA RECUPERACIÓN DE PLÁSTICOS QUEMADOS POR EL SOL Y MANTENIMIENTO DE AQUELLOS QUE SEAN NUEVOS. DA UN EFECTO INCREÍBLE SOBRE LOS PLÁSTICOS EXTERIORES DE SU VEHÍCULO. OTORGA PROLONGADA HUMECTACIÓN EVITANDO EL CUARTEADO PREMATURO. TIENE MUY BUENA RESISTENCIA AL AGUA Y NO SE PEGA LA TIERRA. NO CONTIENE SOLVENTES, ESO ES ALGO QUE LO DIFERENCIA DEL RESTO DE LOS PRODUCTOS!',
                         imagen:'https://i.postimg.cc/g2xV5qk9/GEL-ACONDICIONADOR-TERNNOVA-lion.png',     
                         precio: 1250,
-                        categoria: 'shampoo'
+                        categoria: 'shampoo',
+                        stock: 10
                     },
                 
                     {   id: 3,
@@ -30,7 +31,8 @@ const productos2 = [{
                         descripcion: 'UN PODEROSO DESCONTAMINANTE FÉRRICO AVANZADO DE pH BALANCEADO, CAPAZ DE REMOVER LOS RESIDUOS FÉRRICOS MÁS DUROS. DESARROLLADO PARA DISOLVER EL POLVO DE LOS FRENOS Y OTROS CONTAMINANTES DERIVADOS DEL HIERRO.',
                         imagen: 'https://i.postimg.cc/Z5X8hBCW/IRON-HUNTER-lion.png',
                         precio: 1200,
-                        categoria: 'desengrasante'
+                        categoria: 'desengrasante',
+                        stock: 10
 
                     },
                 
@@ -40,7 +42,8 @@ const productos2 = [{
                         descripcion: 'ES UN INCREÍBLE ACONDICIONADOR DE NEUMÁTICOS, EL CUAL OTORGA BRILLO EXTREMO Y DURADERO. EVITA EL AGRIETAMIENTO DEL NEUMÁTICO DEJÁNDOLO COLOR NEGRO SIN QUE ESTE SE RESEQUE. LOGRÁ EL TAN DESEADO BRILLO EFECTO MOJADO "WET LOOK".',
                         imagen: 'https://i.postimg.cc/QM5pN8T0/TERNNOVA-PETROLUEM-lion.png',
                         precio: 1400,
-                        categoria: 'desengrasante'
+                        categoria: 'desengrasante',
+                        stock: 10
 
                     }
                 ]
@@ -63,7 +66,6 @@ function ItemDetailContainer() {
     }, []);
     return (
                     <div>
-                        <ItemCount initial={1} stock={5}/>
                         <ItemDetail getItem={item} />
                     </div>
     );
